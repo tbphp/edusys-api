264b67d2e9135f2d8159a099fddc40a6bff425eb
tangbo <admin@tbphp.net>
2023-03-11 14:53:00 +0800

    feat:api分页定制化

---
+++ app/Libs/ApiPaginator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Libs;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class ApiPaginator extends LengthAwarePaginator
+{
+    public function __construct($items, $total, $perPage, $currentPage = null, array $options = [])
+    {
+        parent::__construct($items, $total, $perPage, $currentPage, $options);
+    }
+
+    /**
+     * 设置总数
+     * @param int $total
+     * @return $this
+     */
+    public function setTotal(int $total): self
+    {
+        $this->total = $total;
+        $this->lastPage = max((int)ceil($total / $this->perPage()), 1);
+
+        return $this;
+    }
+
+    /**
+     * 设置每页数量
+     * @param int $perPage
+     * @return $this
+     */
+    public function setPerPage(int $perPage): self
+    {
+        $this->perPage = $perPage;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'list' => $this->items->toArray(),
+            'total' => $this->total(),
+            'current_page' => $this->currentPage(),
+            'last_page' => $this->lastPage(),
+            'per_page' => $this->perPage(),
+            'from' => $this->firstItem() ?? 0,
+            'to' => $this->lastItem() ?? 0,
+        ];
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Libs\ApiPaginator;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(LengthAwarePaginator::class, ApiPaginator::class);
     }
 
     /**
