040e737cbcd8a76000e5aba5fbfe5bba606304aa
tangbo <admin@tbphp.net>
2023-03-14 10:58:57 +0800

    chore:HEADER_X_FORWARDED_AWS_ELB

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = '**';
 
     /**
      * The current proxy header mappings.
@@ -20,10 +20,6 @@ class TrustProxies extends Middleware
      * @var array
      */
     protected $headers = [
-        Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_X_FORWARDED_AWS_ELB,
     ];
 }
