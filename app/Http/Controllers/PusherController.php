5dbff9e4395c983af175d5211034bd88b9554f50
tangbo <admin@tbphp.net>
2023-03-15 10:39:48 +0800

    feat:pusher webhook

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
 use Pusher\Pusher;
 use Pusher\PusherException;
 
 class PusherController extends Controller
 {
+    const CACHE_KEY = 'pusher_online';
+
     /**
      * 回调
      *
@@ -30,7 +33,18 @@ class PusherController extends Controller
         $webhook = $pusher->webhook($headers, $request->getContent());
 
         foreach ($webhook->get_events() as $event) {
-            Log::info('pusher_event', $event);
+            switch ($event->name) {
+                case 'channel_occupied':
+                    Redis::sadd(self::CACHE_KEY, [$event->channel]);
+                    Log::info('用户上线：' . $event->channel);
+                    break;
+                case 'channel_vacated':
+                    Redis::srem(self::CACHE_KEY, $event->channel);
+                    Log::info('用户下线：' . $event->channel);
+                    break;
+            }
         }
+
+        Log::info('当前用户数：' . Redis::scard(self::CACHE_KEY));
     }
 }
